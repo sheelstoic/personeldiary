9fece693a17dd9695d3851c24ee6c3e6a77bf9e8
Sheel tripathi <Sheeltripathi@gmail.com>
2026-08-20 15:16:35 +0530

    first function created

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-print("========================")
-print("  PERSONAL DIARY")
-print("========================")
-name = input(" sheel brat tripathi")
-print("\nWelcome,",name)
-print("My personal diary project has started!")
+import json
+import os
+import hashlib
+import secrets
+from datetime import datetime
+from getpass import getpass
 
+class PersonalDiarey:
+   def __intit__(self): 
+      self.data_file = "personal_diary_data.json"
+      self.backup_folder = "diary_backups"
+      self.data = {
+         "users": {},
+         "entries": []
+      }
+      self.current_user = None
+      self.load_data()
+
+
+
+      
